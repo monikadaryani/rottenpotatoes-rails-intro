e5544848b04a288501c392b638f44271b000d907
EC2 Default User <ec2-user@ip-172-31-33-238.us-east-2.compute.internal>
2019-09-27 20:35:44 +0000

    part 0 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def helper_class(field)
+        if(params[:sort].to_s == field)
+            return 'hilite'
+        else
+            return nil
+        end
+    end
 end
