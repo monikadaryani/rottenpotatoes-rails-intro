bd4e5d3161297a4e047ccb90a279173e2e936e66
EC2 Default User <ec2-user@ip-172-31-33-238.us-east-2.compute.internal>
2019-09-27 23:19:28 +0000

    hi

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,11 +4,9 @@ module MoviesHelper
     count.odd? ?  "odd" :  "even"
   end
   def make_yellow_class(field)
-        # if(params[:sort].to_s == field)
+         if(params[:sort].to_s == field)
             return 'hilite'
-        # else
-            # return nil
-        # end
+         end
   end
   #ref : https://stackoverflow.com/questions/18028239/changing-css-properties-of-a-haml-table-from-rails-view
 end
