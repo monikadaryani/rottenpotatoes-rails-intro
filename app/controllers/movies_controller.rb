d7c81bf9ecef118f0cd5ad0be13b5430f0856cf4
EC2 Default User <ec2-user@ip-172-31-33-238.us-east-2.compute.internal>
2019-09-28 17:08:00 +0000

    part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,18 +13,16 @@ class MoviesController < ApplicationController
   def index
     columnName = params[:sort]
     @movies = Movie.order(columnName)
-    index method
     @all_ratings=Movie.uniq.pluck(:rating)
     @ratings_selected = params[:ratings]
     if(@ratings_selected)
-      puts("ratings were selected already wow #{@ratings_selected}")
       @ratings_selected_keys = @ratings_selected.keys
     end
     
     #Below is default ratings(ie:all)
     @ratings_selected_keys||=@all_ratings
     
-    @movies = Movie.where(rating: @ratings_selected_keys).order(sort_column + " " + sort_direction)
+    @movies = Movie.where(rating: @ratings_selected_keys).order(columnName + " " + sort_direction)
     
   end
 
